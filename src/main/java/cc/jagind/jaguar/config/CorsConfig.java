87ad8cc8a0fce740f594a25c6870e3d798463f6d
Julio <ItsMonkey1@gmail.com>
2025-09-23 16:04:53 -0700

    Add 'FRONTEND_URL' env variable and ensure it's used in CorsConfig

--- src/main/java/cc/jagind/jaguar/config/CorsConfig.java
+++ src/main/java/cc/jagind/jaguar/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package cc.jagind.jaguar.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,10 +10,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class CorsConfig implements WebMvcConfigurer {
 
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:5173", "http://localhost:4173", "http://localhost:3000")
+                .allowedOrigins(frontendUrl)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
